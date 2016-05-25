2080bdbf6ffb8cc866bcae7f2d1f85f2dfe83f08
Igor Minar <igor@angularjs.org>
2016-05-25 09:29:51 -0700

    update to the latest ts and types

--- test.ts
+++ test.ts
@@ -1,5 +1,8 @@
 import * as webdriver from 'selenium-webdriver';
 import * as protractor from 'protractor';
+import * as fse from 'fs-extra';
 
 let c = new webdriver.Command("hello");
-let we = new protractor.WebElement();
+let we = new protractor.WebElement(null, null);
+
+fse.appendFile("lala", "baba");
